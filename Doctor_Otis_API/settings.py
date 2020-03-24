1e53dceeefe3cefa972edc2860ff944d91eb945c
cai <jack_czm@vip.sina.com>
2020-03-24 11:26:50 +0800

    update settings.py

--- Doctor_Otis_API/settings.py
+++ Doctor_Otis_API/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#=!5-!mowf(k!j7-k*qxbp+cs($sa1+%nya5-k(eyw)+@^&s24'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
