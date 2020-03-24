4609f031829de71f4b75a003e990d05537c38957
cai <jack_czm@vip.sina.com>
2020-03-24 11:22:00 +0800

    add DO-API GET & POST function

---
+++ API/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from utils.cosmos_db import cosmos
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.http import Http404
+
+
+# Create your views here.
+
+@api_view(['GET', 'POST'])
+def DO_data(request, unit_number):
+    if request.method in ('GET', 'POST'):
+        data = cosmos.query('DO_auto_maintenance_result',
+                            fields=('DO_value',), query_params={'UnitNumber': unit_number})
+        if data:
+            return Response(data[0])
+        else:
+            content = {'error': 'unit number not found'}
+            return Response(content, status=status.HTTP_404_NOT_FOUND)
