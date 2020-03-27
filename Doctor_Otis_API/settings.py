fee158c8d8c7a925ffd7134248371fbb0f3af5cd
cai <jack_czm@vip.sina.com>
2020-03-27 13:31:06 +0800

    update settings.py

--- Doctor_Otis_API/settings.py
+++ Doctor_Otis_API/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#=!5-!mowf(k!j7-k*qxbp+cs($sa1+%nya5-k(eyw)+@^&s24'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -118,3 +118,7 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+try:
+    from local_settings import *
+except:
+    pass
