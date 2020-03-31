0080ee2ceb854ffa85833368038c2f95e5d75066
cai <jack_czm@vip.sina.com>
2020-03-31 18:13:34 +0800

    update settings.py

--- Doctor_Otis_API/settings.py
+++ Doctor_Otis_API/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import pymysql
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -28,6 +29,7 @@ ALLOWED_HOSTS = ['*']
 
 # Application definition
 
+pymysql.install_as_MySQLdb()
 INSTALLED_APPS = [
     'whitenoise.runserver_nostatic',
     'django.contrib.admin',
