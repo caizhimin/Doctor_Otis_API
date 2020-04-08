1108992bc39a8a7970517bce74af097410a6bde7
cai <jack_czm@vip.sina.com>
2020-04-08 10:46:27 +0800

    update setting.py

--- Doctor_Otis_API/settings.py
+++ Doctor_Otis_API/settings.py
@@ -19,7 +19,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ["SECRET_KEY"]
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
@@ -145,3 +146,4 @@ try:
     from local_settings import *
 except:
     pass
+
