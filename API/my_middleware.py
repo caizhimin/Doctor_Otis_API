566577e2571ccda6b53914d1cc4f9448c2109569
cai <jack_czm@vip.sina.com>
2020-04-15 17:37:43 +0800

    update middleware

--- API/my_middleware.py
+++ API/my_middleware.py
@@ -1,7 +1,12 @@
-
 from django.utils import deprecation
 
 
 class CheckSourceMiddleware(deprecation.MiddlewareMixin):
     def process_request(self, request):
-        print(request.POST)
+        meta = request.META
+        HTTP_USER_AGENT = meta.get('HTTP_USER_AGENT')
+        REMOTE_ADDR = meta.get('REMOTE_ADDR')
+        print('REMOTE_ADDR %s' % REMOTE_ADDR, 'HTTP_USER_AGENT: %s' % HTTP_USER_AGENT,  'POST-data: %s'% request.POST)
+
+
+
