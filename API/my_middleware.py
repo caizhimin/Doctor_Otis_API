573d999f57bd5dbed86cfbcba7614340c2762986
cai <jack_czm@vip.sina.com>
2020-04-20 11:16:21 +0800

    update middleware

--- API/my_middleware.py
+++ API/my_middleware.py
@@ -4,9 +4,11 @@ from django.utils import deprecation
 class CheckSourceMiddleware(deprecation.MiddlewareMixin):
     def process_request(self, request):
         meta = request.META
-        HTTP_USER_AGENT = meta.get('HTTP_USER_AGENT')
+        HTTP_USER_AGENT = request.headers.get('User-Agent')
         HTTP_X_FORWARDED_FOR = meta.get('HTTP_X_FORWARDED_FOR')
-        print('HTTP_X_FORWARDED_FOR %s' % HTTP_X_FORWARDED_FOR, 'HTTP_USER_AGENT: %s' % HTTP_USER_AGENT,  'POST-data: %s'% request.POST)
+        Authorization = request.headers.get('Authorization')
+        print('HTTP_X_FORWARDED_FOR %s' % HTTP_X_FORWARDED_FOR, 'HTTP_USER_AGENT: %s' % HTTP_USER_AGENT,
+              'Authorization %s' % Authorization, 'POST-data: %s' % request.POST)
 
 
 
