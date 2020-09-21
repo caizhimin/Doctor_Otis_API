0ee878f5760738dc8ac9338392ca64f481223c72
cai <jack_czm@vip.sina.com>
2020-09-21 10:34:43 +0800

    update DO-api status

--- API/views.py
+++ API/views.py
@@ -9,26 +9,29 @@ from oauth2_provider.oauth2_validators import AccessToken
 from API.models import ApiRecord
 
 
-
 # Create your views here.
 
 @api_view(['POST', 'GET'])
 def DO_data(request, unit_number):
+    meta = request.META
+    HTTP_X_FORWARDED_FOR = meta.get('HTTP_X_FORWARDED_FOR')
+    HTTP_USER_AGENT = request.headers.get('User-Agent')
+    Authorization = request.headers.get('Authorization')
     if request.method in ('GET', 'POST'):
         try:
             data = cosmos.query('DO_auto_maintenance_result',
                                 fields=('DO_value',), query_params={'UnitNumber': unit_number})
         except:
+            ApiRecord.create(client_ip=HTTP_X_FORWARDED_FOR, user_agent=HTTP_USER_AGENT, authorization=Authorization,
+                             unit_number=unit_number, status=-1)
             return Response({'Result': -1, 'Message': '服务器错误，请求失败', 'Data': {}})
         if data:
-            meta = request.META
-            HTTP_X_FORWARDED_FOR = meta.get('HTTP_X_FORWARDED_FOR')
-            HTTP_USER_AGENT = request.headers.get('User-Agent')
-            Authorization = request.headers.get('Authorization')
             ApiRecord.create(client_ip=HTTP_X_FORWARDED_FOR, user_agent=HTTP_USER_AGENT, authorization=Authorization,
-                             unit_number=unit_number)
+                             unit_number=unit_number, status=1)
             return Response({'Result': 0, 'Message': '请求成功', 'Data': data[0]['DO_value']})
         else:
+            ApiRecord.create(client_ip=HTTP_X_FORWARDED_FOR, user_agent=HTTP_USER_AGENT, authorization=Authorization,
+                             unit_number=unit_number, status=0)
             return Response({'Result': 0, 'Message': 'eventlog不存在', 'Data': {}})
 
 
