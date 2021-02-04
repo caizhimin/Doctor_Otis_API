61deaff9164c76eab897ad9e4498713be8e720fe
cai <jack_czm@vip.sina.com>
2021-02-04 10:36:08 +0800

    fix create function

--- API/models.py
+++ API/models.py
@@ -64,11 +64,12 @@ class ApiRecord(models.Model):
                 for i in data.get('autoItems'):
                     item = i['item'].replace('-', '_', 1)
                     tsbStatus = i['tsbStatus']
-                    floorInfo = i['floorInfo']
+                    floorInfo = i['tsbString']
                     exec('record.' + item + '=' + str(tsbStatus))
                     if item in ['A_1_4', 'A_1_5', 'A_1_7', 'A_1_19', 'A_1_20', 'A_1_21', 'A_1_22', 'A_1_23',
                                 'A_1_24', 'A_1_25', 'A_1_28', 'A_2_3', 'A_2_5', 'A_2_8', 'A_3_4', 'A_4_2']:
-                        exec('record.' + item + '_extra_floor' + '=' + (str(floorInfo) if floorInfo else 'None'))
+                        # print('record.' + item + '_extra_floor' + '=' + '%s' % (str(floorInfo) if floorInfo else 'None'))
+                        exec('record.' + item + '_extra_floor' + '=' + "'" + (str(floorInfo) if floorInfo else 'None') + "'")
         record.save()
 
     class Meta:
