1f7001047c48e5d48ee5470370c90210318deb31
jack_czm@vip.sina.com <1q2w3e4r>
2021-12-09 17:01:11 +0800

    fix

--- API/views.py
+++ API/views.py
@@ -26,16 +26,19 @@ def DO_data(request, unit_number):
             return Response({'Result': -1, 'Message': '服务器错误，请求失败', 'Data': {}})
         if data:
             oil = get_unit_oil(unit_number)
-            for i in data[0]['DO_value']['autoItems']:
-                if i['item'] == 'A-1_13':
-                    if oil < 71:
-                        i['tsbStatus'] = 1
-                        reset_unit_oil(unit_number)
-                    else:
-                        i['tsbStatus'] = 0
-            ApiRecord.create(client_ip=HTTP_X_FORWARDED_FOR, user_agent=HTTP_USER_AGENT, authorization=Authorization,
-                             unit_number=unit_number, status=1, data=data[0]['DO_value'])
-            return Response({'Result': 0, 'Message': '请求成功', 'Data': data[0]['DO_value']})
+            if data[0].get('DO_value'):
+                for i in data[0]['DO_value']['autoItems']:
+                    if i['item'] == 'A-1_13':
+                        if oil < 71:
+                            i['tsbStatus'] = 1
+                            reset_unit_oil(unit_number)
+                        else:
+                            i['tsbStatus'] = 0
+                ApiRecord.create(client_ip=HTTP_X_FORWARDED_FOR, user_agent=HTTP_USER_AGENT, authorization=Authorization,
+                                 unit_number=unit_number, status=1, data=data[0]['DO_value'])
+                return Response({'Result': 0, 'Message': '请求成功', 'Data': data[0]['DO_value']})
+            else:
+                return Response({'Result': 0, 'Message': 'eventlog不存在', 'Data': {}})
         else:
             ApiRecord.create(client_ip=HTTP_X_FORWARDED_FOR, user_agent=HTTP_USER_AGENT, authorization=Authorization,
                              unit_number=unit_number, status=0)
