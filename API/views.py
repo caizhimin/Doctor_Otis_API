dfc54494bdfeb95926611cf7906e809995706cd9
jack_czm@vip.sina.com <1q2w3e4r>
2022-01-25 12:51:56 +0800

    fix

--- API/views.py
+++ API/views.py
@@ -1,3 +1,4 @@
+import requests
 from datetime import datetime
 from utils.cosmos_db import cosmos
 from utils.DO_mysql import get_unit_oil, reset_unit_oil
@@ -34,15 +35,172 @@ def DO_data(request, unit_number):
                             reset_unit_oil(unit_number)
                         else:
                             i['tsbStatus'] = 0
-                ApiRecord.create(client_ip=HTTP_X_FORWARDED_FOR, user_agent=HTTP_USER_AGENT, authorization=Authorization,
+                ApiRecord.create(client_ip=HTTP_X_FORWARDED_FOR, user_agent=HTTP_USER_AGENT,
+                                 authorization=Authorization,
                                  unit_number=unit_number, status=1, data=data[0]['DO_value'])
                 return Response({'Result': 0, 'Message': '请求成功', 'Data': data[0]['DO_value']})
             else:
                 return Response({'Result': 0, 'Message': 'eventlog不存在', 'Data': {}})
         else:
-            ApiRecord.create(client_ip=HTTP_X_FORWARDED_FOR, user_agent=HTTP_USER_AGENT, authorization=Authorization,
-                             unit_number=unit_number, status=0)
-            return Response({'Result': 0, 'Message': 'eventlog不存在', 'Data': {}})
+            url = 'https://developerstudio-china.otiselevator.com/iot-core/v2/api/CHN/v2/unitlist'
+            Ocp_Apim_Subscription_Key = 'd7299181f9b94dfb8cfcefbb676a4c1d'
+            headers = {'Ocp-Apim-Subscription-Key': Ocp_Apim_Subscription_Key}
+            req_text = {
+                "CountryCode": "CHN",
+                "PlatformType": "robustel",
+                "PageSize": 20,
+                "FreeTextSearch": unit_number,
+                "FetchCount": False,
+                "commissioningState": "commissioned,fullycommissioned,partiallycommissioned"
+            }
+            res = requests.post(headers=headers, url=url, json=req_text).json()
+            if res.get('units'):
+                new_data = {
+                    "autoItems": [
+                        {
+                            "item": "A-1_7",
+                            "tsbStatus": 0,
+                            "tsbString": "",
+                            "floorInfo": None
+                        },
+                        {
+                            "item": "A-2_3",
+                            "tsbStatus": 0,
+                            "tsbString": "",
+                            "floorInfo": None
+                        },
+                        {
+                            "item": "A-1_21",
+                            "tsbStatus": 0,
+                            "tsbString": "",
+                            "floorInfo": None
+                        },
+                        {
+                            "item": "A-2_8",
+                            "tsbStatus": 0,
+                            "tsbString": "",
+                            "floorInfo": None
+                        },
+                        {
+                            "item": "A-1_22",
+                            "tsbStatus": 0,
+                            "tsbString": "",
+                            "floorInfo": None
+                        },
+                        {
+                            "item": "A-1_28",
+                            "tsbStatus": 0,
+                            "tsbString": "",
+                            "floorInfo": None
+                        },
+                        {
+                            "item": "A-2_5",
+                            "tsbStatus": 0,
+                            "tsbString": "",
+                            "floorInfo": None
+                        },
+                        {
+                            "item": "A-3_4",
+                            "tsbStatus": 0,
+                            "tsbString": "",
+                            "floorInfo": None
+                        },
+                        {
+                            "item": "A-1_24",
+                            "tsbStatus": 80,
+                            "tsbString": "",
+                            "floorInfo": None
+                        },
+                        {
+                            "item": "A-1_23",
+                            "tsbStatus": 80,
+                            "tsbString": "",
+                            "floorInfo": None
+                        },
+                        {
+                            "item": "A-1_20",
+                            "tsbStatus": 80,
+                            "tsbString": "",
+                            "floorInfo": None
+                        },
+                        {
+                            "item": "A-1_19",
+                            "tsbStatus": 80,
+                            "tsbString": "",
+                            "floorInfo": None
+                        },
+                        {
+                            "item": "A-1_5",
+                            "tsbStatus": 80,
+                            "tsbString": "",
+                            "floorInfo": None
+                        },
+                        {
+                            "item": "A-4_2",
+                            "tsbStatus": 80,
+                            "tsbString": "",
+                            "floorInfo": None
+                        },
+                        {
+                            "item": "A-1_4",
+                            "tsbStatus": 80,
+                            "tsbString": "",
+                            "floorInfo": None
+                        },
+                        {
+                            "item": "A-1_6",
+                            "tsbStatus": 1,
+                            "tsbString": "",
+                            "floorInfo": None
+                        },
+                        {
+                            "item": "A-1_13",
+                            "tsbStatus": 80,
+                            "tsbString": "",
+                            "floorInfo": None
+                        },
+                        {
+                            "item": "A-1_1",
+                            "tsbStatus": 0,
+                            "tsbString": "",
+                            "floorInfo": None
+                        },
+                        {
+                            "item": "A-1_25",
+                            "tsbStatus": 0,
+                            "tsbString": "",
+                            "floorInfo": None
+                        },
+                        {
+                            "item": "A-2_2",
+                            "tsbStatus": 0,
+                            "tsbString": "",
+                            "floorInfo": None
+                        },
+                        {
+                            "item": "A-3_5",
+                            "tsbStatus": 0,
+                            "tsbString": "",
+                            "floorInfo": None
+                        },
+                        {
+                            "item": "A-4_4",
+                            "tsbStatus": 0,
+                            "tsbString": "",
+                            "floorInfo": None
+                        }]}
+
+                ApiRecord.create(client_ip=HTTP_X_FORWARDED_FOR, user_agent=HTTP_USER_AGENT,
+                                 authorization=Authorization,
+                                 unit_number=unit_number, status=1, data=new_data)
+                cosmos.insert('DO_auto_maintenance_result',
+                              data={'UnitNumber': unit_number, 'DO_value': new_data})
+                return Response({'Result': 0, 'Message': '请求成功', 'Data': new_data})
+            else:
+                ApiRecord.create(client_ip=HTTP_X_FORWARDED_FOR, user_agent=HTTP_USER_AGENT,
+                                 authorization=Authorization,
+                                 unit_number=unit_number, status=0)
+                return Response({'Result': 0, 'Message': 'eventlog不存在', 'Data': {}})
 
 
 @api_view(['GET'])
